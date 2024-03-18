2d538231c3beee9aaab72f2e0a85ee7cdb05ffb9
Dmitry Klimovsky <dmitryklimovsky05@gmail.com>
2024-03-18 17:19:59 +0300

    initialized project

---
+++ include/fixed_point.hpp
@@ -0,0 +1,14 @@
+#ifndef FIXED_POINT_HPP
+#define FIXED_POINT_HPP
+
+namespace fp {
+
+template<typename T = int, unsigned int FRB = 8>
+struct fixed_point
+{
+  
+};
+
+}
+
+#endif // !FIXED_POINT_HPP 
