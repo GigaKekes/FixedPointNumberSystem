c1ddfefdb006982d0a8e4543f757fc5112018b52
Dmitry Klimovsky <dmitryklimovsky05@gmail.com>
2024-03-21 15:40:20 +0300

    made operations

--- include/fixed_point.hpp
+++ include/fixed_point.hpp
@@ -1,13 +1,161 @@
 #ifndef FIXED_POINT_HPP
 #define FIXED_POINT_HPP
 
+#include <ios>
+#include <limits>
+#include <iostream>
+#include <cmath>
+
 namespace fp {
+
+template <typename T = int>
+constexpr T shiftValue(T value, int amount) noexcept
+{
+  if (value < 0) 
+  {
+    if (amount < 0) 
+    {
+      return ~(~value >> -amount);
+    } 
+    else 
+  {
+      return -(-value << amount);
+    }
+  } 
+  else 
+{
+    if (amount < 0) 
+    {
+      return value >> -amount;
+    } 
+    else
+  {
+      return value << amount;
+    }
+  }
+}
+
 
 template<typename T = int, unsigned int FRB = 8>
 struct fixed_point
 {
-  
+private:
+  // Raw data about this number
+  T raw_;
+public:
+
+  // Static members
+  static const int frac_count = FRB;
+  static const int int_count = std::numeric_limits<T>::digits - FRB;
+
+  // Constructors
+  constexpr fixed_point(const fixed_point &other) = default;
+  fixed_point &operator=(const fixed_point &other) = default;
+  ~fixed_point() = default;
+
+  constexpr fixed_point() noexcept : raw_(0) {}
+
+  constexpr fixed_point(T int_part) noexcept
+  : raw_(shiftValue(int_part, frac_count)) {}
+
+  constexpr fixed_point(T raw_value, int shift) noexcept
+  : raw_(shiftValue(raw_value, shift)) {}
+
+  // Getters
+  inline T raw_value() const noexcept 
+  {
+    return raw_;
+  }
+
+  constexpr T int_part() const noexcept
+  {
+    return shiftValue(raw_, -frac_count);
+  }
+
+  constexpr T frac_part() const noexcept
+  {
+    using ULL = unsigned long long;
+    return raw_ & (frac_count <= 0 ? 0 : int_count <= 0 ? ~0ULL : (1ULL << ULL(frac_count >= 0 ? frac_count : 0)) - 1ULL);
+  }
+
+  // Arithmetics
+  constexpr  fixed_point<T, FRB>& operator += (const fixed_point<T, FRB>& lfp)
+  {
+    raw_ += lfp.raw_value();
+    return *this;
+  }
+  constexpr  fixed_point<T, FRB>& operator -= (const fixed_point<T, FRB>& lfp)
+  {
+    raw_ -= lfp.raw_value();
+    return *this;
+  }
+  constexpr  fixed_point<T, FRB>& operator *= (const fixed_point<T, FRB>& lfp)
+  {
+    raw_ = shiftValue(raw_ * lfp.raw_value(), -fixed_point<T,FRB>::frac_count );
+    return *this;
+  }
+  constexpr  fixed_point<T, FRB>& operator /= (const fixed_point<T, FRB>& lfp)
+  {
+    raw_ = shiftValue(raw_ / lfp.raw_value(), fixed_point<T,FRB>::frac_count );
+    return *this;
+  }
+
+  constexpr fixed_point<T, FRB> operator+ (const fixed_point<T, FRB>& lfp) const
+  {
+    return {raw_ + lfp.raw_value(), 0};
+  }
+  constexpr   fixed_point<T, FRB> operator- (const fixed_point<T, FRB>& lfp) const
+  {
+    return {raw_ - lfp.raw_value(), 0};
+  }
+  constexpr  fixed_point<T, FRB> operator* (const fixed_point<T, FRB>& lfp) const
+  {
+    return {raw_ * lfp.raw_value(), -fixed_point<T,FRB>::frac_count};
+  }
+  constexpr  fixed_point<T, FRB> operator/ (const fixed_point<T, FRB>& lfp) const
+  {
+    return {raw_ / lfp.raw_value(), fixed_point<T,FRB>::frac_count} ;
+  }
+
+  // Comparison
+  constexpr bool operator== (const fixed_point<T, FRB>& lfp) const
+  {
+    return raw_ == lfp.raw_value();
+  }
+  constexpr  bool operator!= (const fixed_point<T, FRB>& lfp) const
+  {
+    return raw_ != lfp.raw_value();
+  }
+  constexpr  bool operator< (const fixed_point<T, FRB>& lfp) const
+  {
+    return raw_ < lfp.raw_value();
+  }
+  constexpr  bool operator> (const fixed_point<T, FRB>& lfp) const
+  {
+    return raw_ > lfp.raw_value();
+  }
+  constexpr  bool operator <= (const fixed_point<T, FRB>& lfp) const
+  {
+    return raw_ <= lfp.raw_value();
+  }
+  constexpr  bool operator >= (const fixed_point<T, FRB>& lfp) const
+  {
+    return raw_ >= lfp.raw_value();
+  }
 };
+
+// Stream
+template <typename T = int, unsigned int FRB = 8>
+inline std::ostream &operator<<(std::ostream &stream, const fixed_point<T, FRB> &fixedNum)
+{
+  //stream << fixedNum.int_part() << "+" << fixedNum.frac_part() << "*2^-" << fixed_point<T, FRB>::frac_count;
+
+  T frac = static_cast<T>(fixedNum.frac_part() * std::pow(5, fixed_point<T, FRB>::frac_count));
+  while(frac % 10 == 0 && frac != 0) frac /= 10; 
+
+  stream << fixedNum.int_part() << "." << frac;
+  return stream;
+}
 
 }
 
--- main.cpp
+++ main.cpp
@@ -1,4 +1,37 @@
-int main (int argc, char *argv[]) {
-  
+#include "include/fixed_point.hpp"
+#include <iostream>
+
+template <typename T = int, unsigned int FRB = 8>
+void operatorTest(fp::fixed_point<T,FRB> fp1, fp::fixed_point<T,FRB> fp2)
+{
+  std::cout << "Fixed point number 1: " << fp1 << " " << fp1.raw_value() << std::endl; 
+  std::cout << "Fixed point number 2: " << fp2 << " " << fp2.raw_value() << std::endl; 
+  std::cout<< "sum = " << fp1+fp2 << std::endl;
+  std::cout<< "dif = " << fp1-fp2 << std::endl;
+  std::cout<< "prod = " << fp1*fp2 << std::endl;
+  std::cout<< "frac = " << fp1/fp2 << std::endl;
+  std::cout<< fp1 << " == " << fp2 << "(" << (fp1 == fp2) << ")" << std::endl;  
+  std::cout<< fp1 << " != " << fp2 << "(" << (fp1 != fp2) << ")" << std::endl;  
+  std::cout<< fp1 << " <= " << fp2 << "(" << (fp1 <= fp2) << ")" << std::endl;  
+  std::cout<< fp1 << " >= " << fp2 << "(" << (fp1 >= fp2) << ")" << std::endl;  
+  std::cout<< fp1 << " < " << fp2 << "(" << (fp1 < fp2) << ")" << std::endl;  
+  std::cout<< fp1 << " > " << fp2 << "(" << (fp1 > fp2) << ")" << std::endl;  
+}
+
+int main (int argc, char *argv[]) 
+{
+  fp::fixed_point<int, 2> fp2(103, 0);
+  fp::fixed_point<int, 2> fp1(203,0);
+  //operatorTest(fp1,fp2);
+
+  fp::fixed_point<int, 8> fpeq1(10123, 4);
+  fp::fixed_point<int, 8> fpeq2(1234, 4);
+  //operatorTest(fpeq1, fpeq2);
+
+  fp::fixed_point<int, 8> fpneg1(-100, 4);
+  fp::fixed_point<int, 8> fpneg2(26, 4);
+  operatorTest(fpneg1, fpneg2);
+
+
   return 0;
 }
